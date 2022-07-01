b6ed4ee887215251386032143719fb60937f9015
wyoung163 <20201014@sungshin.ac.kr>
2022-07-02 00:25:23 +0900

    Initial commit

---
+++ src/main/java/lunar/BlueBell/BlueBellApplication.java
@@ -0,0 +1,13 @@
+package lunar.BlueBell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lunar/BlueBell/BlueBellApplicationTests.java
@@ -0,0 +1,13 @@
+package lunar.BlueBell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
